d1a013c7bda9d9175eab6cbb757bdccb4a232108
Deniz <dnzilkay1@gmail.com>
2025-03-20 19:18:39 +0300

    listeler hakkında çalışma

---
+++ 04_listeler_demelter_sozlukler.py
@@ -0,0 +1,29 @@
+# Liste tanımlama
+meyveler = ["Elma", "Armut", "Muz", "Çilek"]
+
+# Listeye eleman ekleme
+meyveler.append("Portakal")
+
+# Belirli bir elemanı değiştirme
+meyveler[1] = "Karpuz"
+
+# Elemanları yazdırma
+print(meyveler)
+
+
+# Demet tanımlama
+renkler = ("Kırmızı", "Mavi", "Yeşil")
+
+# Elemanlara erişim
+print(renkler[0])  # Kırmızı
+
+
+# Sözlük tanımlama
+kisiler = {"Ahmet": 25, "Mehmet": 30, "Zeynep": 22}
+
+# Değere erişim
+print(kisiler["Ahmet"])  # 25
+
+# Yeni eleman ekleme
+kisiler["Ayşe"] = 28
+
